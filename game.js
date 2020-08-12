1ac5aae11972f2b72cb4edab61e84273ddb73317
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-13 00:49:59 +0530

    bouncing movement up and down

--- game.js
+++ game.js
@@ -14,16 +14,37 @@ H = canvas.height = 700;
 //creating canvas Context
 pen = canvas.getContext('2d');
 
+//creating fighter object
+fighter = {
+    x:140,
+    y:100,
+    w:50,
+    h:50,
+    speed : 10,
+    color: "green"
+}
+
 }
 
 
 function draw(){
+   //clear the old frame
+   pen.clearRect(0,0,W,H)
 
+    pen.fillStyle = fighter.color; 
+    pen.fillRect(fighter.x , fighter.y , fighter.w , fighter.h)
 
 }
 
 function update() {
     
+    // go in reverse if it reaches its boundaries
+    if(fighter.y > H-fighter.h || fighter.y < 0 ){
+        fighter.speed *= -1;
+    }
+    //movement of fighter up and down
+    fighter.y += fighter.speed;
+
 
 }
 
@@ -34,5 +55,6 @@ function gameLoop() {
     update()
 }
 
+load_images()
 init()
 var f = setInterval(gameLoop , 100)
