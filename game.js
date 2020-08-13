d8ff0f1542c2ee984a19e66b7cf71fd30fdabe92
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-13 23:15:02 +0530

    added assests file

--- game.js
+++ game.js
@@ -14,8 +14,8 @@ H = canvas.height = 700;
 //creating canvas Context
 pen = canvas.getContext('2d');
 
-//creating fighter object
-fighter = {
+//creating Enemies
+Enemy1 = {
     x:140,
     y:100,
     w:50,
@@ -24,26 +24,59 @@ fighter = {
     color: "green"
 }
 
+Enemy2 = {
+    x: Enemy1.x +300,
+    y: Enemy1.y+100,
+    w: Enemy1.w,
+    h: Enemy1.h,
+    speed :Enemy1.speed + 4,
+    color :Enemy1.color
+}
+
+Enemy3 = {
+    x: Enemy2.x +200,
+    y: Enemy2.y+100,
+    w: Enemy2.w,
+    h: Enemy1.h,
+    speed :Enemy2.speed +2,
+    color :Enemy2.color
+    
+}
+
 }
 
 
 function draw(){
    //clear the old frame
    pen.clearRect(0,0,W,H)
+    pen.fillStyle = Enemy2.color;
 
-    pen.fillStyle = fighter.color; 
-    pen.fillRect(fighter.x , fighter.y , fighter.w , fighter.h)
+    pen.fillRect(Enemy1.x, Enemy1.y , Enemy1.w , Enemy1.h)
+    pen.fillRect(Enemy2.x, Enemy2.y , Enemy2.w , Enemy2.h)
+    pen.fillRect(Enemy3.x, Enemy3.y , Enemy3.w , Enemy3.h)
 
 }
 
 function update() {
     
-    // go in reverse if it reaches its boundaries
-    if(fighter.y > H-fighter.h || fighter.y < 0 ){
-        fighter.speed *= -1;
+    if(Enemy1.y > H - Enemy1.h || Enemy1.y < 0 ){
+        Enemy1.speed *= -1;
     }
     //movement of fighter up and down
-    fighter.y += fighter.speed;
+    Enemy1.y += Enemy1.speed;
+
+    if(Enemy2.y > H - Enemy2.h || Enemy2.y < 0 ){
+        Enemy2.speed *= -1
+    }
+    //movement of fighter up and down
+    Enemy2.y += Enemy2.speed;
+
+    // go in reverse if it reaches its boundaries
+    if(Enemy3.y > H - Enemy3.h || Enemy3.y < 0 ){
+        Enemy3.speed *= -1;
+    }
+    //movement of fighter up and down
+    Enemy3.y += Enemy3.speed;
 
 
 }
